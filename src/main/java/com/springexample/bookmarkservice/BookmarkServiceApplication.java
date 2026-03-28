8a007b67ff61ee9ef4a438070af70e2aceb69f86
sudharsan.pattabiraman <sudharsan.pattabiraman@anywhere.co>
2026-03-28 12:59:51 +0530

    cleaning up the root

--- src/main/java/com/springexample/bookmarkservice/BookmarkServiceApplication.java
+++ src/main/java/com/springexample/bookmarkservice/BookmarkServiceApplication.java
@@ -1,4 +1,4 @@
-package com.spring_example.bookmark_service;
+package com.springexample.bookmarkservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
